03855f73928a0f0781260b5db3da07a30452e5fd
Pedro PGR <20152523@itla.edu.do>
2020-10-15 12:29:35 -0400

    Removing redux logger from production

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -26,9 +26,9 @@ const Header: React.FC = () => {
                 <Link className="option" to="/shop">
                     SHOP
                 </Link>
-                <Link className="option" to="/shop">
+                {/* <Link className="option" to="/shop">
                     CONTACT
-                </Link>
+                </Link> */}
                 {currentUser ?
                     <div
                         className="option"
