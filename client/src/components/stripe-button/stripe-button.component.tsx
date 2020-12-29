e9ec82b593cdca7c25c1b60fdedd8085d11ba3ad
Pedro_Shanlatte_PGR <pedro.shanlatte@pgr.gob.do>
2020-12-29 11:41:54 -0400

    adding server side

--- client/src/components/stripe-button/stripe-button.component.tsx
+++ client/src/components/stripe-button/stripe-button.component.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 import { IStripeCheckout } from '../../data-types/stripe-types'
 
@@ -9,8 +9,21 @@ const StripeCheckoutButton: React.FC<IStripeCheckout> = ({ price }) => {
     const publishableKey = 'pk_test_51HcWudGZJjAs4PtjBv6iLAlBFFXHa0V1EtYrjyXaaZLq0dbBK51QLNFshxDCRcqZL74kz2oVUc4TzWNPssxVEWRK00aLHb6vjF'
 
     const onToken = (token: any) => {
-        console.log(token);
-        alert('Payment Successful')
+        axios({
+            url: 'http://localhost:5000/payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment Successful')
+        }).catch(error => {
+            console.log("Payment Error: ", error);
+            alert(
+                'There was an issue with your payment'
+            );
+        })
     }
 
     return (
