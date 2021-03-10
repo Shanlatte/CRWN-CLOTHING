f6bf5d1a5fb2b9f21155f556d532146b8fa13d09
Pedro_Shanlatte_PGR <pedro.shanlatte@pgr.gob.do>
2021-03-10 10:43:10 -0400

    fixing production pwa structure

--- server.js
+++ server.js
@@ -12,24 +12,24 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true}))
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-    // app.use(enforce.HTTPS({ trustProtoHeader: true }));
+    app.use(compression);
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-    })
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
 }
 
 app.listen(port, error => {
     if (error) throw error;
-    console.log('Server running on port ' + port);
+    console.log('Server is running on port ' + port);
 });
 
 app.get('/service-worker.js', (req, res) => {
