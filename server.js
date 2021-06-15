2050c21d84f6df426365c70caf0fc93895a4a715
Pedro <20152523@itla.edu.do>
2021-06-15 19:00:35 -0400

    removing cors

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
@@ -15,7 +14,6 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(compression);
