3132f24d9484106df8676640669f31df52b37187
Pedro <20152523@itla.edu.do>
2021-06-15 17:42:29 -0400

    Fix homePage error

--- client/src/pages/homepage/homepage.component.tsx
+++ client/src/pages/homepage/homepage.component.tsx
@@ -3,7 +3,6 @@ import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 
 const HomePage: React.FC = () => {
-    throw Error
     return (
         <div className="homepage">
             <Directory />
